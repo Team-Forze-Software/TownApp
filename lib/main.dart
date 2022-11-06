9a36de3d93a2df561320cbec0254258f62694296
Ji2mys <ji2mys@gmail.com>
2022-11-06 08:20:13 -0500

    Firebase setup, without secrets

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:town_app/firebase_options.dart';
 import 'package:town_app/pages/splash_page.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform
+  );
   runApp(const MyApp());
 }
 
